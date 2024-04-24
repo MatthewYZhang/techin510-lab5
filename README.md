8387cbaae7deef8ebc528ec4a5d3e5ab101ae02a
matt <matthew.yz@qq.com>
2024-04-23 20:47:46 -0700

    finish lab5

--- README.md
+++ README.md
@@ -5,22 +5,19 @@ Matthew Zhang
 ## Getting Started
 
 ```bash
-source venv/Scripts/activate
 pip install -r requirements.txt
 streamlit run app.py
 ```
 
 ## Deployment Page
 
-https://techin510-lab4-93kqwznexnwo8nbusrjsci.streamlit.app/
+
 
 ## What I learned
 
-1. How to interact with Postgres
-2. How to manipulate data on the front end
-3. How to build OOP class
-4. How to do the web scrap using bs and request
+1. How to use Gemini API
+2. How to build prompts
 
 ## Questions
 
-1. Is there a better way to build queries?
+N/A
--- app.py
+++ app.py
@@ -3,34 +3,54 @@ import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 import streamlit as st
+import time
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel('gemini-pro')
 
 prompt_template = """
-You are an expert at planning oversea trips.
+You are an expert at help others modify their resume to fit the job they want to apply for.
 
-Please take the users request and plan a comprehensive trip for them.
+First, analyze the user's resume fitness to the job on a scale of 1 to 10, 1 is not fit, and 10 is a complete fit.
 
-Please include the following details:
-- The destination
-- The duration of the trip
-- The activities that will be done
-- The accommodation
+Then, give out possible improvements of the resume. For example, if the resume says "I worked with my manager to enhance the robustness of the scripts", you can suggest the following improvement in this format:
+Original: I worked with my manager to enhance the robustness of the scripts
+Improved: I reduced the downtime of this specific script by 85% time collaborating with my manager
+Why: include metrics to enhance contribution
 
-The user's request is:
-{prompt}
+For every point, seperate them using different indentation. 
+
+The user's resume is:
+{resume}
+
+The job description is:
+{description}
 """
 
-def generate_content(prompt):
-    response = model.generate_content(prompt)
+def generate_content(resume, job_description):
+    response = model.generate_content(prompt_template.format(resume=resume, description=job_description))
     return response.text
 
-st.title("🏝️ AI Travel Planning")
+def stream_output():
+    for word in reply.split(" "):
+        yield word + " "
+        time.sleep(0.02)
 
-prompt = st.text_area("Enter your next travel request (days, destination, activities, etc.):")
-if st.button("Give me a plan!"):
-    reply = generate_content(prompt)
-    st.write(reply)
+st.title("Resume Builder")
+
+c1, c2 = st.columns(2)
+
+with c1:
+    resume = st.text_area("Enter your current resume", height=300)
+with c2:
+    job_description = st.text_area("Enter the job description of the job you want to apply to", height=300)
+
+reply = None
+
+if st.button("Make my resume better!", use_container_width=True):
+    reply = generate_content(resume, job_description)
+    # print(reply, type(reply))
+    # st.write(reply)
+    st.write_stream(stream_output)
